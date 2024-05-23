da6176f34c6cab9be93cf886155d95b9a3fda95b
Zavier Miller <zavierjmiller@gmail.com>
2024-05-22 23:36:53 -0400

    update return type for more general support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
 export { default as useFormValidation } from './use-form-validation';
-
-export type ValidationErrors<T> = Record<keyof T, string>;
--- src/use-form-validation.ts
+++ src/use-form-validation.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import type { ZodType } from 'zod';
+import type { ZodFormattedError, ZodType } from 'zod';
 
 /**
  * This hook is used to validate form data using a Zod schema.
@@ -7,25 +7,19 @@ import type { ZodType } from 'zod';
  * @returns An object with a `validate` function that takes the form data and returns an object with the errors for each field, or null if the data is valid
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function useValidation<TZod extends ZodType<any, any, any>>(
+export default function useFormValidation<TZod extends ZodType<any, any, any>>(
   zodSchema: TZod
 ) {
   type Data = z.infer<TZod>;
-  return (data: Data): Record<keyof Data, string> | null => {
-    // Validate the data
-    const result = zodSchema.safeParse(data);
+  return (data: Data): ZodFormattedError<Data> | null => {
+    // Have to use a weird `as` hack here to get types working properly
+    const result = (zodSchema as ZodType<Data>).safeParse(data);
     if (result.success) {
       // If the data is valid, return null
       return null;
     }
 
     // If the data is invalid, return an object with the errors for each field
-    return result.error.errors.reduce(
-      (acc, error) => ({
-        ...acc,
-        [error.path[0]!]: error.message,
-      }),
-      {} as Record<keyof Data, string>
-    );
+    return result.error.format();
   };
 }
